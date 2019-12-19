f3c3a62ca86320a86a285b459e0b904cec5b0cbc
joshilita <52619112+joshilita@users.noreply.github.com>
2019-12-19 15:28:24 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+echo "Welcome to the Official NewOS Co. install file!"
+echo "Do you want to continue? (Yes/No)"
+read install
+if [ $install = yes ];then
+echo "Installing. Please wait..."
+sleep 4
+apt install git -y
+git pull https://github.com/joshilita/newOS
+echo "Done! Have fun!"
+else
+echo "cancel"
+fi
