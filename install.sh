e404bc2ef55fd74b1a6513e8be3602a6d54da813
joshilita <52619112+joshilita@users.noreply.github.com>
2019-12-19 22:27:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ $install = yes ];then
 echo "Installing. Please wait..."
 sleep 4
 apt install git -y
-git pull https://github.com/joshilita/newOS
+git pull https://github.com/joshilita/newOS/
 echo "Done! Have fun!"
 else
 echo "cancel"
